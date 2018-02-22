19a08f1e9a55b28ae05000aba744bf2dcb03ee35
Andrew Crites <acrites@mobiquityinc.com>
2018-02-22 18:37:53 -0500

    fix(style): Adding styles to Home example

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,16 +1,34 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
-import { View, Text } from 'react-native';
+import { StyleSheet, ViewStyle, View, Text, TextStyle } from 'react-native';
 
 export class HomeComponent extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Testing</Text>
+      <View style={styles.container}>
+        <Text style={styles.textContent}>Edit or remove this file.</Text>
       </View>
     );
   }
 }
 
-export const Home = connect(undefined, undefined)(HomeComponent);
+export const Home = connect()(HomeComponent);
+
+interface Style {
+  container: ViewStyle;
+  textContent: TextStyle;
+}
+
+const styles = StyleSheet.create<Style>({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    paddingBottom: 100,
+  },
+  textContent: {
+    fontSize: 25,
+    textAlign: 'center',
+  },
+});
