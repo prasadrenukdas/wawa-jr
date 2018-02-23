601964490bd672bab763437db168db76c6d36c10
Andrew Crites <acrites@mobiquityinc.com>
2018-02-23 09:10:37 -0500

    fix(tests): fixing tests after breaking changes

--- src/__tests__/Home.test.tsx
+++ src/__tests__/Home.test.tsx
@@ -1,11 +1,11 @@
 import 'react-native';
 import * as React from 'react';
-import { App } from 'src/App';
+import { HomeComponent } from 'src/Home';
 
 // Note: test renderer must be required after react-native.
 import { create } from 'react-test-renderer';
 
 it('renders correctly', () => {
-  const tree = create(<App />);
+  const tree = create(<HomeComponent />);
   tree;
 });
